f0a9d5587cba335a5ac10606e37319bca61bbfd2
Rafael Fernandes <rafaelbenzi@estudante.ufscar.br>
2024-05-10 17:38:00 +0200

    last update

--- script_tp2_org.py
+++ script_tp2_org.py
@@ -1,17 +1,5 @@
 import numpy as np
-from skimage.morphology import (
-    erosion,
-    dilation,
-    binary_erosion,
-    opening,
-    closing,
-    white_tophat,
-    reconstruction,
-    black_tophat,
-    skeletonize,
-    convex_hull_image,
-    thin,
-)
+from skimage.morphology import erosion, dilation, binary_erosion, opening, closing, white_tophat, reconstruction, skeletonize
 from skimage.morphology import square, diamond, octagon, rectangle, star, disk
 from skimage.filters.rank import entropy, enhance_contrast_percentile
 from PIL import Image
@@ -20,26 +8,6 @@ from skimage.util import img_as_ubyte
 import math
 from skimage import data, filters
 from matplotlib import pyplot as plt
-
-se1 = square(6)  # square
-se2 = diamond(5)  # diamond
-se3 = np.random.randint(2, size=(6, 6))
-se4 = disk(8)
-se5 = np.array(
-    [
-        [1, 1, 1, 1, 1],
-        [1, 1, 0, 1, 1],
-        [1, 0, 0, 0, 1],
-        [1, 1, 0, 1, 1],
-        [1, 1, 1, 1, 1],
-    ],
-    dtype=np.uint8,
-)
-se6 = np.array([[1, 0, 1, 0, 1]], dtype=np.bool_)
-se7 = np.array([[1], [0], [1]], dtype=np.bool_)
-
-structure_elements = [se1, se2, se3, se4, se5, se6, se7]
-
 
 def my_segmentation(img, img_mask, seuil):
     img_out = img_mask & (img < seuil)
@@ -69,18 +37,6 @@ def switch(operator_counter):
         operator = "Top Hat Ouverture"
     elif operator_counter == 4:
         operator = "Top Hat Fermeture"
-    elif operator_counter == 5:
-        operator = "Ouverture after Gradient"
-    elif operator_counter == 6:
-        operator = "Fermeture after Gradient"
-    elif operator_counter == 7:
-        operator = "Ouverture after Gradient Erosion"
-    elif operator_counter == 8:
-        operator = "Fermeture after Gradient Erosion"
-    elif operator_counter == 9:
-        operator = "Ouverture after Gradient Dilation"
-    elif operator_counter == 10:
-        operator = "Fermeture after Gradient Dilation"
     else:
         return -1
     return operator
@@ -89,59 +45,37 @@ def switch(operator_counter):
 def ImAllOperations(img, se):
     imgs = []
     imDil = dilation(img, se)  # Dilatation morphologique
-    imEro = erosion(img, se)  # Érosion morpholigique
+    imEro = erosion(img, se)  # Érosion morphologique
     imOuv = opening(img, se)  # Ouverture morphologique
     imFerm = closing(img, se)  # Fermeture morphologique
 
     gm = imDil - imEro  # Gradient morphologique
     imgs.append(gm)
-    gminus = img - imEro  # Gradient morphologique erosion
+    gminus = img - imEro  # Gradient intérieur
     imgs.append(gminus)
-    gplus = imDil - img  # Gradient morphologique dilation
+    gplus = imDil - img  # Gradient extérieur
     imgs.append(gplus)
-    imTopHat1 = img - imOuv  # Top Hat ouverture
+    imTopHat1 = img - imOuv  # Top Hat
     imgs.append(imTopHat1)
-    imTopHat2 = imFerm - img  # Top Hat fermeture
+    imTopHat2 = imFerm - img  # Top Hat conjugué
     imgs.append(imTopHat2)
-    operationTest1 = opening(gm)  # Ouverture après Gradient
-    imgs.append(operationTest1)
-    operationTest2 = closing(gm)  # Fermeture après Gradient
-    imgs.append(operationTest2)
-    operationTest3 = closing(gminus)  # Fermeture après Gradient morphologique erosion
-    imgs.append(operationTest3)
-    operationTest4 = opening(gminus)  # Ouverture après Gradient morphologique erosion
-    imgs.append(operationTest4)
-    operationTest5 = closing(gplus)  # Fermeture après Gradient morphologique dilation
-    imgs.append(operationTest5)
-    operationTest6 = opening(gplus)  # Ouverture après Gradient morphologique dilation
-    imgs.append(operationTest6)
 
     return imgs
 
 
-def evaluate_all(image, structural_elements):
+def evaluate_operation(image, image_mask, structural_elements):
     best_f1 = 0
-    best_accuracy = 0
-    best_recall = 0
-    for seuil in [14]:
+    for seuil in range(5,10):
         for se in structural_elements:
             images = ImAllOperations(image, se)
             count_operator = 0
             for img in images:
-                if count_operator != 11:
-                    img[invalid_pixels] = 0
-                    img_seg = my_segmentation(img, img_mask, seuil)
-                    img_seg = np.logical_not(img_seg)
-                    img_seg[invalid_pixels] = 0
-                else:
-                    img_seg = img
+                img_seg = my_segmentation(img, image_mask, seuil)
+                img_seg = np.logical_not(img_seg)
+                img_seg[invalid_pixels] = 0
 
-                ACCU, RECALL, img_skel, GT_skel = evaluate(
-                    img_seg.astype(np.int8), img_GT.astype(np.int8)
-                )
-
-                print(seuil, "Accuracy =", ACCU, ", Recall =", RECALL)
-
+                ACCU, RECALL, img_skel, GT_skel = evaluate(img_seg.astype(np.int8), img_GT.astype(np.int8))
+                #print(f"Seuil = {seuil}, Accuracy = {ACCU}, Recall = {RECALL}")
                 f1score = 2*ACCU * RECALL / (ACCU + RECALL)
 
                 if f1score > best_f1:
@@ -154,79 +88,87 @@ def evaluate_all(image, structural_elements):
                     best_operator = switch(count_operator)
                     best_seuil = seuil
                     best_skel = img_skel
-
                 count_operator += 1
-    return (
-        best_f1,
-        best_accuracy,
-        best_recall,
-        best_operator,
-        best_se,
-        best_seuil,
-        best_image,
-        best_imageseg,
-        best_skel,
-        GT_skel,
-    )
 
+    return best_f1, best_accuracy, best_recall, best_operator, best_se, best_seuil, best_image, best_imageseg, best_skel, GT_skel
+
+
+def evaluate_reconstruction(img):
+    best_f1 = 0
+    for i in range(1,5):
+        for j in range(1,8,2):
+            rec = reconstruction(erosion(img, square(i)), img, footprint=square(j))
+            ACCU, RECALL, img_out_skel, _ = evaluate(rec.astype(np.uint8), img_GT.astype(np.uint8))
+            f1_score = 2 * ACCU * RECALL / (ACCU + RECALL)
+            if f1_score > best_f1:
+                best_f1 = f1_score
+                best_si, best_sj = i, j
+                best_image = rec
+                best_skel = img_out_skel
+
+    return best_f1, best_si, best_sj, best_image, best_skel
+
+
+# Définition des éléments structurants
+squares = [square(i) for i in range(4,8)]
+diamands = [diamond(i) for i in range(4,8)]
+se3 = np.random.randint(2, size=(6, 6))
+se4 = disk(8)
+se5 = np.array([[1, 1, 1, 1, 1],
+                [1, 1, 0, 1, 1],
+                [1, 0, 0, 0, 1],
+                [1, 1, 0, 1, 1],
+                [1, 1, 1, 1, 1],], dtype=np.uint8)
+se6 = np.array([[1, 0, 1]], dtype=np.bool_)
+se7 = np.array([[1], [0], [1]], dtype=np.bool_)
+
+structure_elements = [se3, se4, se5, se6, se7] + squares + diamands
+
+img_number = [('01', 'OSC'), ('02', 'OSC'), ('03', 'OSN'), ('08', 'OSN'), ('21', 'OSC'), ('26', 'ODC'), ('28', 'ODN'), ('32', 'ODC'), ('37', 'ODN'), ('48', 'OSN')]
 
 # Ouvrir l'image originale en niveau de gris
-img_original = np.asarray(Image.open("./TP2/images_IOSTAR/star08_OSN.jpg")).astype(
-    np.uint8
-)
+img_original = np.asarray(Image.open("./images_IOSTAR/star02_OSC.jpg")).astype(np.uint8)
 
 nrows, ncols = img_original.shape
 row, col = np.ogrid[:nrows, :ncols]
 
 # On ne considere que les pixels dans le disque inscrit
 img_mask = (np.ones(img_original.shape)).astype(np.bool_)
-invalid_pixels = (row - nrows / 2) ** 2 + (col - ncols / 2) ** 2 > (
-    (nrows - 20) / 2
-) ** 2
+invalid_pixels = (row - nrows / 2) ** 2 + (col - ncols / 2) ** 2 > ((nrows - 10) / 2) ** 2
 img_mask[invalid_pixels] = 0
 
 # Ouvrir l'image Verite Terrain en booleen
-img_GT = np.asarray(Image.open("./TP2/images_IOSTAR/GT_08.png")).astype(np.bool_)
+img_GT = np.asarray(Image.open("./images_IOSTAR/GT_02.png")).astype(np.bool_)
 
-(
-    f1_score,
-    best_accuracy,
-    best_recall,
-    melhorop,
-    melhorse,
-    best_seuil,
-    melhorimg,
-    melhorimgseg,
-    best_skel,
-    GT_skel,
-) = evaluate_all(img_original, structure_elements)
+# Operation de filtre moyenne sur l'image originale
+img_filtered = filters.rank.mean(img_original, square(3))
 
-print(f"Best f1: {f1_score}, Acc: {best_accuracy}, Rec: {best_recall}")
-print(best_seuil)
-print(melhorse)
-print(melhorop)
+# Évaluation de la meilleure operation, le meilleur élément structurant, le meilleur seuil et les images résultantes
+f1_score, best_accuracy, best_recall, best_op, best_se, best_seuil, result_image, result_imag_seg, result_skel, GT_skel = evaluate_operation(img_filtered, img_mask, structure_elements)
 
-rec = reconstruction(erosion(melhorimgseg, square(3)), melhorimgseg, footprint=square(3))
+print(f"\nBest f1: {f1_score}, Acc: {best_accuracy}, Rec: {best_recall}")
+print(f"Best Treshold for Segmentation: {best_seuil}")
+print(f"Best Structure Element: \n{best_se}")
+print(f"Best Operation: {best_op}")
 
-ACCU, RECALL, img_out_skel, GT_skel = evaluate(rec.astype(np.uint8), img_GT.astype(np.uint8))
-f1_score = 2*ACCU * RECALL / (ACCU + RECALL)
+# Évaluation de la meilleure opération de reconstruction
+f1_score_rec, best_si, best_sj, best_rec, result_skel_rec = evaluate_reconstruction(result_imag_seg)
 
-print(f"Reconstruction -> F1: {f1_score}, Acc: {ACCU}, Rec: {RECALL}")
+print(f"\nReconstruction -> F1: {f1_score_rec}, square({best_si}), foot print = square({best_sj})")
 
-
-plt.suptitle(melhorop)
+plt.suptitle(best_op)
 plt.subplot(231)
-plt.imshow(img_original, cmap="gray")
+plt.imshow(img_original)
 plt.title("Image Originale")
 plt.subplot(232)
-plt.imshow(melhorimg)
-plt.title("Segmentation")
+plt.imshow(result_imag_seg)
+plt.title(f"Meilleure Résultat F-mesure: {f1_score*100:.2f}%")
 plt.subplot(233)
-plt.imshow(melhorimgseg)
-plt.title("Segmentation binary")
+plt.imshow(result_skel)
+plt.title("Segmentation Squelette")
 plt.subplot(234)
-plt.imshow(best_skel)
-plt.title("Segmentation squelette")
+plt.imshow(result_image)
+plt.title(f"Operation : {best_op}")
 plt.subplot(235)
 plt.imshow(img_GT)
 plt.title("Verite Terrain")
@@ -236,9 +178,37 @@ plt.title("Verite Terrain Squelette")
 plt.show()
 
 plt.subplot(121)
-plt.imshow(melhorimgseg)
-plt.title("Antes")
+plt.imshow(result_imag_seg)
+plt.title(f"Sans Reconstruction F-masure:{f1_score*100:.2f}%")
 plt.subplot(122)
-plt.imshow(rec)
-plt.title("Reconstruction")
+plt.imshow(best_rec)
+plt.title(f"Avec Reconstruction F-masure:{f1_score_rec*100:.2f}%")
 plt.show()
+
+# Résultats pour toutes les images
+for number in img_number:
+    img_original = np.asarray(Image.open(f"./images_IOSTAR/star{number[0]}_{number[1]}.jpg")).astype(np.uint8)
+    img_GT = np.asarray(Image.open(f"./images_IOSTAR/GT_{number[0]}.png")).astype(np.bool_)
+    # Operation de filtre moyenne sur l'image
+    img_filtered = filters.rank.mean(img_original, square(3))
+
+    # Évaluation de la meilleure operation, le meilleur élément structurant, le meilleur seuil et les images résultantes
+    f1_score, best_accuracy, best_recall, best_op, best_se, best_seuil, result_image, result_imag_seg, result_skel, GT_skel = evaluate_operation(img_filtered, img_mask, structure_elements)
+    print(f"\nImage: star{number[0]}_{number[1]}.jpg:")
+    print(f"\nBest f1: {f1_score}, Acc: {best_accuracy}, Rec: {best_recall}")
+    print(f"Best Treshold for Segmentation: {best_seuil}")
+    print(f"Best Structure Element: \n{best_se}")
+    print(f"Best Operation: {best_op}")
+
+    # Évaluation de la meilleure opération de reconstruction
+    f1_score_rec, best_si, best_sj, best_rec, result_skel_rec = evaluate_reconstruction(result_imag_seg)
+
+    print(f"\nReconstruction -> F1: {f1_score_rec}, square({best_si}), foot print = square({best_sj})")
+    plt.subplot(121)
+    plt.imshow(best_rec)
+    plt.title(f"Meilleure Résultat - F-mesure: {f1_score_rec*100:.2f}%")
+    plt.subplot(122)
+    plt.imshow(img_GT)
+    plt.title("Verite Terrain")
+    plt.suptitle(f"Comparaison Entre Résultat et Objectif - Image: star{number[0]}_{number[1]}.jpg")
+    plt.show()
--- script_tp2_test.py
+++ script_tp2_test.py
@@ -9,24 +9,25 @@ import math
 from skimage import data, filters
 from matplotlib import pyplot as plt
 
-se1 = square(6) # square
+se1 = square(5) # square
 
-se2 = diamond(5) # diamond
+se2 = diamond(7) # diamond
 
-#se3  = np.ones((3)) # line
-se3 = np.random.randint(2, size=(6, 6))
+se3 = np.random.randint(2, size=(4, 4))
 
-se4 = disk(8)
+se4 = disk(4)
 
-se5 = np.array([[1, 1, 1, 1, 1],
-                [1, 1, 0, 1, 1],
-                [1, 0, 0, 0, 1],
-                [1, 1, 0, 1, 1],
-                [1, 1, 1, 1, 1]], dtype=np.uint8)
+se5 = np.array([[1, 0, 0, 0, 1],
+                [0, 1, 0, 1, 0],
+                [0, 0, 1, 0, 0],
+                [0, 1, 0, 1, 0],
+                [1, 0, 0, 0, 1]], dtype=np.uint8)
 
 se6 = np.array([[1, 0, 1, 0, 1]], dtype=np.bool_)
 
 se7 = np.array([[1], [0], [1]], dtype=np.bool_)
+
+se8 = octagon(4, 3)
 
 structure_elements = [se1, se2, se3, se4, se5, se6, se7]
 print(type(structure_elements))
@@ -38,13 +39,16 @@ def my_segmentation(img, img_mask, seuil):
 def evaluate(img_out, img_GT):
     GT_skel = skeletonize(img_GT) # On reduit le support de l'evaluation...
     img_out_skel = skeletonize(img_out) # ...aux pixels des squelettes
+    all_trues = np.sum(img_GT == True)
     TP = np.sum(img_out_skel & img_GT) # Vrais positifs
     FP = np.sum(img_out_skel & ~img_GT) # Faux positifs
     FN = np.sum(GT_skel & ~img_out) # Faux negatifs
 
     ACCU = TP / (TP + FP) # Precision
     RECALL = TP / (TP + FN) # Rappel
-    return ACCU, RECALL, img_out_skel, GT_skel
+    real_eval = 2*ACCU*RECALL/(ACCU + RECALL)
+
+    return ACCU, RECALL, img_out_skel, GT_skel, real_eval
 
 def switch(operator_counter):
     if operator_counter == 0:
@@ -57,6 +61,18 @@ def switch(operator_counter):
         operator = 'Top Hat Ouverture'
     elif operator_counter == 4: 
         operator = 'Top Hat Fermeture'
+    elif operator_counter == 5: 
+        operator = 'Ouverture after Gradient'
+    elif operator_counter == 6: 
+        operator = 'Fermeture after Gradient'
+    elif operator_counter == 7: 
+        operator = 'Ouverture after Gradient Erosion'
+    elif operator_counter == 8: 
+        operator = 'Fermeture after Gradient Erosion'
+    elif operator_counter == 9: 
+        operator = 'Ouverture after Gradient Dilation'
+    elif operator_counter == 10: 
+        operator = 'Fermeture after Gradient Dilation'
     else:
         return -1
     return operator
@@ -69,8 +85,8 @@ def ImAllOperations(img, se):
     imgs = []
     imDil = dilation(img, se) # Dilatation morphologique
     imEro = erosion(img, se) # Érosion morpholigique
-    imOuv = dilation(imEro, se) # Ouverture morphologique
-    imFerm = erosion(imDil, se) # Fermeture morphologique
+    imOuv = opening(img, se) # Ouverture morphologique
+    imFerm = closing(img, se) # Fermeture morphologique
 
     gm = imDil - imEro # Gradient morphologique
     imgs.append(gm)
@@ -80,18 +96,30 @@ def ImAllOperations(img, se):
     imgs.append(gplus)
     imTopHat1 = img - imOuv # Top Hat ouverture
     imgs.append(imTopHat1)
-    imTopHat2 = imFerm # Top Hat fermeture
+    imTopHat2 = imFerm - img # Top Hat fermeture
     imgs.append(imTopHat2)
+    operationTest1 = opening(gm) # Ouverture après Gradient
+    imgs.append(operationTest1)
+    operationTest2 = closing(gm) # Fermeture après Gradient
+    imgs.append(operationTest2)
+    operationTest3 = closing(gminus) # Fermeture après Gradient morphologique erosion
+    imgs.append(operationTest3)
+    operationTest4 = opening(gminus) # Ouverture après Gradient morphologique erosion
+    imgs.append(operationTest4)
+    operationTest5 = closing(gplus) # Fermeture après Gradient morphologique dilation
+    imgs.append(operationTest5)
+    operationTest6 = opening(gplus) # Ouverture après Gradient morphologique dilation
+    imgs.append(operationTest6)
     return imgs
 
-imgs = ImAllOperations(img_original, se1)
-gm, gminus, gplus, imTopHat1, imTopHat2 = imgs[0], imgs[1], imgs[2], imgs[3], imgs[4]
+#imgs = ImAllOperations(img_original, se1)
+#gm, gminus, gplus, imTopHat1, imTopHat2 = imgs[0], imgs[1], imgs[2], imgs[3], imgs[4]
 
 nrows, ncols = img_original.shape
 row, col = np.ogrid[:nrows, :ncols]
 #On ne considere que les pixels dans le disque inscrit 
 img_mask = (np.ones(img_original.shape)).astype(np.bool_)
-invalid_pixels = ((row - nrows/2)**2 + (col - ncols/2)**2 > (nrows / 2)**2)
+invalid_pixels = ((row - nrows/2)**2 + (col - ncols/2)**2 > ((nrows-10) / 2)**2)
 img_mask[invalid_pixels] = 0
 
 img_out = my_segmentation(img_original,img_mask,80)
@@ -105,30 +133,48 @@ def evaluate_all(image, structural_elements, treshold):
         images = ImAllOperations(image, se)
         count_operator = 0
         for img in images:
-            img[invalid_pixels] = 0
             img_seg = my_segmentation(img, img_mask, treshold)
+            img_seg = np.logical_not(img_seg)
+            img_seg[invalid_pixels] = 0
 
-            ACCU, RECALL, img_skel, GT_skel = evaluate(img_seg.astype(np.int8), img_GT.astype(np.int8))
-            print('Accuracy =', ACCU,', Recall =', RECALL)
+            ACCU, RECALL, img_skel, GT_skel, real_eval = evaluate(img_seg.astype(np.int8), img_GT.astype(np.int8))
+            print('Accuracy =', ACCU,', Recall =', RECALL, ', Eval = ', real_eval)
             #plt.imshow(img_seg)
             #plt.show()
-            if ACCU > best_accuracy:
-                best_accuracy = ACCU
+            if real_eval > best_accuracy:
+                best_accuracy = real_eval
                 best_se = se
                 best_image = img_seg
+                best_skel = img_skel
                 best_operator = switch(count_operator)
             count_operator += 1
-    return best_accuracy, best_se, best_operator, best_image
+    return best_accuracy, best_se, best_operator, best_image, best_skel, GT_skel
     
-acuracy, melhorse, melhorop, melhorimg = evaluate_all(img_original, structure_elements, 75)
+img_contrast = enhance_contrast_percentile(img_original, footprint=square(5))
+img_filtered = filters.rank.mean(img_contrast, square(1))
+plt.subplot(121)
+plt.imshow(img_original)
+plt.subplot(122)
+plt.imshow(img_filtered)
+plt.show()
+
+acuracy, melhorse, melhorop, melhorimg, melhorskel, skel = evaluate_all(img_filtered, structure_elements, 20)
+#melhorimg_reconstruct = reconstruction(erosion(melhorimg, se1), melhorskel, footprint=se1)
+
 print(acuracy)
 print(melhorse)
 print(melhorop)
+plt.subplot(221)
 plt.imshow(melhorimg)
+plt.title(f"Meilleure Rappel : {acuracy*100:.2f}%")
+plt.subplot(222)
+plt.imshow(melhorskel)
+plt.subplot(223)
+plt.imshow(img_GT)
+plt.title("Verite Terrain")
+plt.subplot(224)
+plt.imshow(skel)
 plt.show()
-
-ACCU, RECALL, img_out_skel, GT_skel = evaluate(img_out.astype(np.int8), img_GT.astype(np.int8))
-print('Accuracy =', ACCU,', Recall =', RECALL)
 
 '''
 plt.subplot(231)
